ceeba68de7b502fbaf1d5aab3337ec4a2613cbe9
Your Name <you@example.com>
2017-06-19 21:05:09 +0200

    ok

--- src/source/TestMain.java
+++ src/source/TestMain.java
@@ -4,7 +4,7 @@ public class TestMain {
 
 	public static void main(String[] args) {
 		System.out.println("Works");
-		System.out.println("My own Branch dmiedzik");b
+		System.out.println("My own Branch dmiedzik");a
 	}
 
 }
